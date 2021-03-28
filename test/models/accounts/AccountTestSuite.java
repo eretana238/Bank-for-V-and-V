ceb319de2c419c0ef70eabe1a00a26aee8b37f24
NathanA0507 <NathanA0507@gmail.com>
2021-03-28 15:33:40 -0600

    Added Test Suite

---
+++ test/models/accounts/AccountTestSuite.java
@@ -0,0 +1,11 @@
+package models.accounts;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+
+@Suite.SuiteClasses({CreditTest.class, CheckingTest.class, SavingsTest.class})
+
+public class AccountTestSuite {
+}
